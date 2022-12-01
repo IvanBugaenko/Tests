53dea45743eccc015703d062922921fc9fa1f4af
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2022-12-01 23:56:15 +0600

    pjhkdgjhlkghl;kdl;hkdf;lkh

--- SpaceBattle.Lib/Commands/StartMoveCommand.cs
+++ SpaceBattle.Lib/Commands/StartMoveCommand.cs
@@ -1,21 +1,5 @@
 namespace SpaceBattle.Lib;
 
-// class StartCommand: ICommand
-// {
-//     private IStartable obj;
-//     public StartCommand(IStartable obj)
-//     {
-//         this.obj = obj;
-//     }  
-
-//     public void Execute()
-//     {
-//         obj.Properties.ToList().ForEach(x => IoC.Resolve<ICommand>("Game.OperationsSetProperty", obj.Target, x.Key, x.Value).Execute());
-//         ICommand cmd = IoC.Resolve<ICommand>("Game.Movement", obj.Target);
-//         IoC.Resolve<ICommand>("Game.Queue.Push", IoC.Resolve<Queue<ICommand>>("Game.Queue"), cmd).Execute();
-//     }
-// }
-
 public class StartMoveCommand: ICommand
 {
     private IStartable obj;
@@ -26,11 +10,11 @@ public class StartMoveCommand: ICommand
 
     public void Execute()
     {
-        // IoC.Resolve<ICommand>("Game.Commands.SetProperty", obj.Target, "Speed", obj.Speed).Execute();
-
         obj.Properties.ToList().ForEach(o => IoC.Resolve<ICommand>("Game.Commands.SetProperty", obj.Target, o.Key, o.Value).Execute());
 
         ICommand cmd = IoC.Resolve<ICommand>("Game.Operations.Moving", obj.Target);
+
+        IoC.Resolve<ICommand>("Game.Commands.SetProperty", obj.Target, "Moving", cmd).Execute();
         
         IoC.Resolve<ICommand>("Game.Queue.Push", IoC.Resolve<Queue<ICommand>>("Game.Queue"), cmd).Execute();
     }
